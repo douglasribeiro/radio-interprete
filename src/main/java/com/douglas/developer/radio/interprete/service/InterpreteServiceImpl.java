c139cc9daffcb16065c45e3a9594ad83f64ee289
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2023-12-20 20:24:57 -0300

    correçaõ de import

--- src/main/java/com/douglas/developer/radio/interprete/service/InterpreteServiceImpl.java
+++ src/main/java/com/douglas/developer/radio/interprete/service/InterpreteServiceImpl.java
@@ -2,7 +2,6 @@ package com.douglas.developer.radio.interprete.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
